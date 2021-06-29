24b96b68a779c1056dbf39964a6fe9b5f1a61502
Cuong Trinh <cuongtrinhq@gmail.com>
2021-06-29 10:18:08 +0700

    first commit - init project

---
+++ onlinebookshop/src/main/java/com/example/onlinebookshop/OnlinebookshopApplication.java
@@ -0,0 +1,14 @@
+package com.example.onlinebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookshopApplication.class, args);
+		System.out.println("This is my assignment to build e-commerce webapp");
+	}
+
+}
